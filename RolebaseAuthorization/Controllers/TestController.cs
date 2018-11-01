7ced545cd7782c0ecc6ca7e846f89bfc9a320938
Hanbing <askhanbing@gmail.com>
2018-11-01 16:14:55 -0500

    Add project files.

---
+++ RolebaseAuthorization/Controllers/TestController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+namespace RolebaseAuthorization.Controllers
+{
+    public class TestController : Controller
+    {
+        [Authorize(Roles = "Admin")]
+        public IActionResult OnlyAdminAccess()
+        {
+            ViewData["role"] = "Admin";
+            return View("MyPage");
+        }
+
+        [Authorize(Roles = "User,Admin,User")]
+        public IActionResult OnlyUserAccess()
+        {
+            ViewData["role"] = "User";
+            return View("MyPage");
+        }
+        [Authorize(Roles = "Staff")]
+        public IActionResult OnlyHRAccess()
+        {
+            ViewData["role"] = "Staff";
+            return View("MyPage");
+        }
+        
+    }
+}
